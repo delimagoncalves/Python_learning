6624bdf2c496953534200538597f652dfe007175
Gilson <>
2022-02-17 19:03:44 -0300

    Exemplo simples de utilização do laço while

---
+++ contadorWhile.py
@@ -0,0 +1,11 @@
+'''Exemplo de utilização do laço While'''
+
+print("Início do Programa")
+
+cont = 1
+
+while cont <= 10:#Executa o laço While enquanto cont for menor ou igual a 10
+    print(cont)
+    cont+=1#A cada iteração do laço adicionar +1 ao contador
+
+print("Fim do programa")
