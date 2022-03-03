10094d589b0c178cb5c93056fcd0e4df7740c898
Gilson <>
2022-03-03 11:15:57 -0300

    Organização em pastas

---
+++ sintaxegeral/lacoFor.py
@@ -0,0 +1,20 @@
+'''
+Exemplo de utilização do laço for
+'''
+
+#este laço exibe até o valor 9 
+for i in range(1,10):
+    print("Número: ",i)
+
+print("")
+
+#variação do laço for, que exibe a cada 2 números
+for i in range(1,10,2):
+    print("Número: ",i)
+
+print("")
+
+
+#variação do laço for, que decrementa o valor inicial
+for i in range(10,1,-2):
+    print("Número: ",i)
